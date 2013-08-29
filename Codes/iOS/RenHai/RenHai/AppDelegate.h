ea128228cb6a856f6d061fe224d5a39eebc60c24
simplelife-patrick <simplelife.patrick@gmail.com>
2013-08-29 18:21:31 +0800

    新建Xcode工程

---
+++ Codes/iOS/RenHai/RenHai/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RenHai
+//
+//  Created by Patrick Deng on 13-8-29.
+//  Copyright (c) 2013年 Simplelife Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Codes/iOS/RenHai/RenHai/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RenHai
+//
+//  Created by Patrick Deng on 13-8-29.
+//  Copyright (c) 2013年 Simplelife Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
