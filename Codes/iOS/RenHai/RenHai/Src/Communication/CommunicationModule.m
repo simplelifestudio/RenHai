c97ff277e490763573922743bc5007f4663bc2b5
simplelife-patrick <simplelife.patrick@gmail.com>
2013-09-01 11:44:22 +0800

    新增Communication模块类和GUI模块类

---
+++ Codes/iOS/RenHai/RenHai/Src/Communication/CommunicationModule.m
@@ -0,0 +1,133 @@
+//
+//  CommunicationModule.m
+//  RenHai
+//
+//  Created by Patrick Deng on 13-9-1.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import "CommunicationModule.h"
+
+#import "Reachability.h"
+
+#import "GUIModule.h"
+
+#define REACHABILITY_HOST @"www.apple.com"
+#define  _HUD_DISPLAY 1
+
+@interface CommunicationModule()
+{
+
+}
+
+@end
+
+@implementation CommunicationModule
+
+SINGLETON(CommunicationModule)
+
+-(void) initModule
+{
+    [self setModuleIdentity:NSLocalizedString(@"Communication Module", nil)];
+    [self.serviceThread setName:NSLocalizedString(@"Communication Module Thread", nil)];
+    [self setKeepAlive:FALSE];
+}
+
+- (void)registerReachability
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(reachabilityChanged:)
+                                                 name:kReachabilityChangedNotification
+                                               object:nil];
+    
+    Reachability* hostReach = [Reachability reachabilityWithHostname:REACHABILITY_HOST];
+    [hostReach startNotifier];    
+}
+
+- (void) unregisterReachability
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)reachabilityChanged:(NSNotification *)note
+{
+    Reachability* curReach = [note object];
+    NSParameterAssert([curReach isKindOfClass: [Reachability class]]);
+    NetworkStatus status = [curReach currentReachabilityStatus];
+    [curReach connectionRequired];
+
+    GUIModule* guiModule = [GUIModule sharedInstance];
+    
+    switch (status)
+    {
+        case NotReachable:
+        {
+            [guiModule showHUD:NSLocalizedString(@"Internet Disconnected", nil) delay:_HUD_DISPLAY];
+            
+            DDLogWarn(@"App's reachability changed to 'NotReachable'.");
+            break;
+        }
+        case ReachableViaWiFi:
+        {
+            [guiModule showHUD:NSLocalizedString(@"WiFi Connected", nil) delay:_HUD_DISPLAY];
+            
+            DDLogWarn(@"App's reachability changed to 'ReachableViaWiFi'.");
+            break;
+        }
+        case ReachableViaWWAN:
+        {
+            [guiModule showHUD:NSLocalizedString(@"3G/GPRS Connected", nil) delay:_HUD_DISPLAY];
+            
+            DDLogWarn(@"App's reachability changed to 'ReachableViaWWAN'.");
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
+
+-(void) releaseModule
+{
+    [self unregisterReachability];
+    
+    [super releaseModule];
+}
+
+-(void) startService
+{
+    DDLogVerbose(@"Module:%@ is started.", self.moduleIdentity);
+    
+    [super startService];
+}
+
+
+-(void) processService
+{
+    MODULE_DELAY
+    
+    [self registerReachability];
+}
+
+-(void)applicationWillResignActive:(UIApplication *)application
+{
+    
+}
+
+-(void)applicationDidEnterBackground:(UIApplication *)application
+{
+
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    
+}
+
+-(void)applicationWillEnterForeground:(UIApplication *)application
+{
+
+}
+
+@end
---
+++ Codes/iOS/RenHai/RenHai/Src/GUI/GUIStyle.m
@@ -0,0 +1,85 @@
+//
+//  GUIStyle.m
+//  Seeds
+//
+//  Created by Patrick Deng on 13-5-2.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import "GUIStyle.h"
+
+#import <QuartzCore/QuartzCore.h>
+
+@implementation GUIStyle
+
+// Static block
++(void) initialize
+{
+}
+
++(void) formatFlatUIButton:(FUIButton*) button buttonColor:(UIColor*) buttonColor shadowColor:(UIColor*) shadowColor shadowHeight:(CGFloat) shadowHeight cornerRadius:(CGFloat) cornerRadius titleColor:(UIColor*) titleColor highlightedTitleColor:(UIColor*) highlightedTitleColor
+{
+    if (button)
+    {
+        button.buttonColor = buttonColor;
+        button.shadowColor = shadowColor;
+        button.shadowHeight = shadowHeight;
+        button.cornerRadius = cornerRadius;
+        // button.titleLabel.font = [UIFont flatFontOfSize:24];
+        [button setTitleColor:titleColor forState:UIControlStateNormal];
+        [button setTitleColor:highlightedTitleColor forState:UIControlStateHighlighted];
+    }
+}
+
++(void) formatFlatUILabel:(UILabel*) label textColor:(UIColor *)textColor
+{
+    if (label)
+    {
+        label.textColor = textColor;
+    }
+}
+
++(void) formatFlatUIProgressView:(UIProgressView*) progress
+{
+    if (progress)
+    {
+        [progress configureFlatProgressViewWithTrackColor:FLATUI_COLOR_PROGRESS_TRACK progressColor:FLATUI_COLOR_PROGRESS];
+    }
+}
+
++(void) formatFlatUINavigationBar:(UINavigationBar*) navigationBar
+{
+    if (navigationBar)
+    {
+        [navigationBar configureFlatNavigationBarWithColor:FLATUI_COLOR_NAVIGATIONBAR];
+        navigationBar.titleTextAttributes = @{//UITextAttributeFont: [UIFont boldFlatFontOfSize:18],
+                                                                        UITextAttributeTextColor: FLATUI_COLOR_BUTTON_TEXT};
+    }
+}
+
++(void) formatFlatUIBarButtonItem:(UIBarButtonItem*) buttonItem
+{
+    if (buttonItem)
+    {
+        [buttonItem configureFlatButtonWithColor:FLATUI_COLOR_BARBUTTONITEM highlightedColor:FLATUI_COLOR_BARBUTTONITEM_HIGHLIGHTED cornerRadius:FLATUI_CORNER_RADIUS];
+        [buttonItem removeTitleShadow];
+    }
+}
+
++(void) formatFlatUIToolbar:(UIToolbar*) toolbar
+{
+    if (toolbar)
+    {
+        [toolbar configureFlatToolbarWithColor:FLATUI_COLOR_TOOLBAR];
+    }
+}
+
++(void) formatFlatUITableViewCell:(UITableViewCell*) cell backColor:(UIColor*) backColor selectedColor:(UIColor*) selectedColor style:(UITableViewCellStyle)style reuseIdentifier:(NSString*)reuseIdentifier
+{
+    if (cell)
+    {
+        [UITableViewCell configureFlatCellWithColor:backColor selectedColor:selectedColor style:style reuseIdentifier:reuseIdentifier];
+    }
+}
+
+@end
