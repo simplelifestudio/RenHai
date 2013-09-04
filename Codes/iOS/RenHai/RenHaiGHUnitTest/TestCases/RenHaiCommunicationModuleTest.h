75d66694fcff86ef1a0cdd76e4f655d829f4e044
simplelife-patrick <simplelife.patrick@gmail.com>
2013-09-04 22:11:27 +0800

    新增通信模块测试类

---
+++ Codes/iOS/RenHai/RenHaiGHUnitTest/TestCases/RenHaiCommunicationModuleTest.h
@@ -0,0 +1,13 @@
+//
+//  RenHaiCommunicationModuleTest.h
+//  RenHai
+//
+//  Created by Patrick Deng on 13-9-4.
+//  Copyright (c) 2013年 Simplelife Studio. All rights reserved.
+//
+
+#import "GHTestCase.h"
+
+@interface RenHaiCommunicationModuleTest : GHTestCase
+
+@end
---
+++ Codes/iOS/RenHai/RenHaiGHUnitTest/TestCases/RenHaiCommunicationModuleTest.m
@@ -0,0 +1,13 @@
+//
+//  RenHaiCommunicationModuleTest.m
+//  RenHai
+//
+//  Created by Patrick Deng on 13-9-4.
+//  Copyright (c) 2013年 Simplelife Studio. All rights reserved.
+//
+
+#import "RenHaiCommunicationModuleTest.h"
+
+@implementation RenHaiCommunicationModuleTest
+
+@end
