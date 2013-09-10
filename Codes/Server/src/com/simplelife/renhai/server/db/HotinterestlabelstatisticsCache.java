022ae0b506c031a0cb29bf7d2333af0484519bb0
Tomas <simplelife.tomas@gmail.com>
2013-09-10 19:30:22 +0800

    为了更新消息字典，临时性提交。正在开发设备同步消息，未完成。

---
+++ Codes/Server/src/com/simplelife/renhai/server/db/HotinterestlabelstatisticsCache.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * 
+ * History:
+ *     2013-9-10: Tomas Chen, initial version
+ * 
+ * Copyright (c) 2013 SimpleLife Studio. All rights reserved.
+ */
+package com.simplelife.renhai.server.db;
+
+import com.simplelife.renhai.server.util.IDbCache;
+import org.slf4j.Logger;
+public class HotinterestlabelstatisticsCache extends Hotinterestlabelstatistics implements IDbCache
+{
+	@Override
+	public boolean saveToDb()
+	{
+		Logger logger = DBModule.instance.getLogger();
+		try
+		{
+			HotinterestlabelstatisticsDAO dao = new HotinterestlabelstatisticsDAO();
+			dao.save(this);
+			return true;
+		}
+		catch(Exception e)
+		{
+			logger.error(e.getMessage());
+			return false;
+		}
+	}
+    
+    @Override
+	public void cache()
+	{
+		DAOWrapper.cache(this);
+	}
+}
+
---
+++ Codes/Server/src/com/simplelife/renhai/server/util/IDbCache.java
@@ -0,0 +1,22 @@
+/**
+ * IDbOperation.java
+ * 
+ * History:
+ *     2013-8-28: Tomas Chen, initial version
+ * 
+ * Copyright (c) 2013 SimpleLife Studio. All rights reserved.
+ */
+
+
+
+package com.simplelife.renhai.server.util;
+
+
+/** */
+public interface IDbCache
+{
+    /** */
+    public boolean saveToDb();
+    
+    public void cache();
+}
