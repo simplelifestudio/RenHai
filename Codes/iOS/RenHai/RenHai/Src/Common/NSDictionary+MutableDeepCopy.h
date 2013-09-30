d167dac4e9ee29582b90c4dfe63d77674c88f5ec
Patrick Deng <simplelife.patrick@gmail.com>
2013-09-30 22:36:33 +0800

    新增NSDictionary的深度复制Category

---
+++ Codes/iOS/RenHai/RenHai/Src/Common/NSDictionary+MutableDeepCopy.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+MutableDeepCopy.h
+//  RenHai
+//
+//  Created by DENG KE on 13-9-30.
+//  Copyright (c) 2013年 Simplelife Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (MutableDeepCopy)
+
+-(NSMutableDictionary*) mutableDeepCopy;
+
+@end
