dcca6c34ec3fb53948139df94b0c4710e1d26166
Patrick Deng <simplelife.patrick@gmail.com>
2013-11-09 16:13:43 +0800

    ServerInterestLabelsViewLayout还有问题暂时关闭（如果Cell为0则Header也会消失）

--- Codes/iOS/RenHai/RenHai/Src/GUI/ServerInterestLabelsViewLayout.m
+++ Codes/iOS/RenHai/RenHai/Src/GUI/ServerInterestLabelsViewLayout.m
@@ -103,80 +103,167 @@
     return result;
 }
 
-- (BOOL) shouldInvalidateLayoutForBoundsChange:(CGRect)newBound
-{
-    return YES;
-}
+//- (BOOL) shouldInvalidateLayoutForBoundsChange:(CGRect)newBound
+//{
+//    return YES;
+//}
+//
+//- (NSArray *) layoutAttributesForElementsInRect:(CGRect)rect
+//{
+//    NSMutableArray *answer = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
+//    
+//    NSMutableIndexSet *missingSections = [NSMutableIndexSet indexSet];
+//    for (NSUInteger idx=0; idx<[answer count]; idx++)
+//    {
+//        UICollectionViewLayoutAttributes *layoutAttributes = answer[idx];
+//        
+//        if (layoutAttributes.representedElementCategory == UICollectionElementCategoryCell)
+//        {
+//            [missingSections addIndex:layoutAttributes.indexPath.section];  // remember that we need to layout header for this section
+//        }
+//        if ([layoutAttributes.representedElementKind isEqualToString:UICollectionElementKindSectionHeader])
+//        {
+//            [answer removeObjectAtIndex:idx];  // remove layout of header done by our super, we will do it right later
+//            idx--;
+//        }
+//    }
+//    
+//    // layout all headers needed for the rect using self code
+//    [missingSections enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop)
+//    {
+//        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:idx];
+//        UICollectionViewLayoutAttributes *layoutAttributes = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader atIndexPath:indexPath];
+//        [answer addObject:layoutAttributes];
+//    }];
+//    
+//    return answer;
+//}
+//
+//- (UICollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+//{
+//    UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+//    if ([kind isEqualToString:UICollectionElementKindSectionHeader])
+//    {
+//        UICollectionView * const cv = self.collectionView;
+//        CGPoint const contentOffset = cv.contentOffset;
+//        CGPoint nextHeaderOrigin = CGPointMake(INFINITY, INFINITY);
+//        
+//        if (indexPath.section+1 < [cv numberOfSections])
+//        {
+//            UICollectionViewLayoutAttributes *nextHeaderAttributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:[NSIndexPath indexPathForItem:0 inSection:indexPath.section+1]];
+//            nextHeaderOrigin = nextHeaderAttributes.frame.origin;
+//        }
+//        
+//        CGRect frame = attributes.frame;
+//        if (self.scrollDirection == UICollectionViewScrollDirectionVertical)
+//        {
+//            frame.origin.y = MIN(MAX(contentOffset.y, frame.origin.y), nextHeaderOrigin.y - CGRectGetHeight(frame));
+//        }
+//        else
+//        {
+//            // UICollectionViewScrollDirectionHorizontal
+//            frame.origin.x = MIN(MAX(contentOffset.x, frame.origin.x), nextHeaderOrigin.x - CGRectGetWidth(frame));
+//        }
+//        attributes.zIndex = 1024;
+//        attributes.frame = frame;
+//    }
+//    return attributes;
+//}
+//
+//- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+//{
+//    UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+//    return attributes;
+//}
+//
+//- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+//{
+//    UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+//    return attributes;
+//}
 
-- (NSArray *) layoutAttributesForElementsInRect:(CGRect)rect
-{
+- (NSArray *)layoutAttributesForElementsInRect3:(CGRect)rect {
+    
     NSMutableArray *answer = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
+    UICollectionView * const cv = self.collectionView;
+    CGPoint const contentOffset = cv.contentOffset;
     
     NSMutableIndexSet *missingSections = [NSMutableIndexSet indexSet];
-    for (NSUInteger idx=0; idx<[answer count]; idx++)
-    {
-        UICollectionViewLayoutAttributes *layoutAttributes = answer[idx];
-        
+    for (UICollectionViewLayoutAttributes *layoutAttributes in answer) {
         if (layoutAttributes.representedElementCategory == UICollectionElementCategoryCell) {
-            [missingSections addIndex:layoutAttributes.indexPath.section];  // remember that we need to layout header for this section
+            [missingSections addIndex:layoutAttributes.indexPath.section];
         }
+    }
+    for (UICollectionViewLayoutAttributes *layoutAttributes in answer) {
         if ([layoutAttributes.representedElementKind isEqualToString:UICollectionElementKindSectionHeader]) {
-            [answer removeObjectAtIndex:idx];  // remove layout of header done by our super, we will do it right later
-            idx--;
+            [missingSections removeIndex:layoutAttributes.indexPath.section];
         }
     }
     
-    // layout all headers needed for the rect using self code
-    [missingSections enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop)
-    {
+    [missingSections enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
+        
         NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:idx];
+        
         UICollectionViewLayoutAttributes *layoutAttributes = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader atIndexPath:indexPath];
+        
         [answer addObject:layoutAttributes];
+        
     }];
     
-    return answer;
-}
-
-- (UICollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
-{
-    UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
-    if ([kind isEqualToString:UICollectionElementKindSectionHeader])
-    {
-        UICollectionView * const cv = self.collectionView;
-        CGPoint const contentOffset = cv.contentOffset;
-        CGPoint nextHeaderOrigin = CGPointMake(INFINITY, INFINITY);
+    for (UICollectionViewLayoutAttributes *layoutAttributes in answer) {
         
-        if (indexPath.section+1 < [cv numberOfSections])
-        {
-            UICollectionViewLayoutAttributes *nextHeaderAttributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:[NSIndexPath indexPathForItem:0 inSection:indexPath.section+1]];
-            nextHeaderOrigin = nextHeaderAttributes.frame.origin;
+        if ([layoutAttributes.representedElementKind isEqualToString:UICollectionElementKindSectionHeader]) {
+            
+            NSInteger section = layoutAttributes.indexPath.section;
+            NSInteger numberOfItemsInSection = [cv numberOfItemsInSection:section];
+            
+            NSIndexPath *firstObjectIndexPath = [NSIndexPath indexPathForItem:0 inSection:section];
+            NSIndexPath *lastObjectIndexPath = [NSIndexPath indexPathForItem:MAX(0, (numberOfItemsInSection - 1)) inSection:section];
+            
+            BOOL cellsExist;
+            UICollectionViewLayoutAttributes *firstObjectAttrs;
+            UICollectionViewLayoutAttributes *lastObjectAttrs;
+            
+            if (numberOfItemsInSection > 0) { // use cell data if items exist
+                cellsExist = YES;
+                firstObjectAttrs = [self layoutAttributesForItemAtIndexPath:firstObjectIndexPath];
+                lastObjectAttrs = [self layoutAttributesForItemAtIndexPath:lastObjectIndexPath];
+            } else { // else use the header and footer
+                cellsExist = NO;
+                firstObjectAttrs = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+                                                                        atIndexPath:firstObjectIndexPath];
+                lastObjectAttrs = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionFooter
+                                                                       atIndexPath:lastObjectIndexPath];
+                
+            }
+            
+            CGFloat topHeaderHeight = (cellsExist) ? CGRectGetHeight(layoutAttributes.frame) : 0;
+            CGFloat bottomHeaderHeight = CGRectGetHeight(layoutAttributes.frame);
+            CGRect frameWithEdgeInsets = UIEdgeInsetsInsetRect(layoutAttributes.frame,
+                                                               cv.contentInset);
+            
+            CGPoint origin = frameWithEdgeInsets.origin;
+            
+            origin.y = MIN(
+                           MAX(
+                               contentOffset.y + cv.contentInset.top,
+                               (CGRectGetMinY(firstObjectAttrs.frame) - topHeaderHeight)
+                               ),
+                           (CGRectGetMaxY(lastObjectAttrs.frame) - bottomHeaderHeight)
+                           );
+            
+            layoutAttributes.zIndex = 1024;
+            layoutAttributes.frame = (CGRect){
+                .origin = origin,
+                .size = layoutAttributes.frame.size
+            };
+            
         }
         
-        CGRect frame = attributes.frame;
-        if (self.scrollDirection == UICollectionViewScrollDirectionVertical)
-        {
-            frame.origin.y = MIN(MAX(contentOffset.y, frame.origin.y), nextHeaderOrigin.y - CGRectGetHeight(frame));
-        }
-        else
-        { // UICollectionViewScrollDirectionHorizontal
-            frame.origin.x = MIN(MAX(contentOffset.x, frame.origin.x), nextHeaderOrigin.x - CGRectGetWidth(frame));
-        }
-        attributes.zIndex = 1024;
-        attributes.frame = frame;
     }
-    return attributes;
-}
-
-- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
-{
-    UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
-    return attributes;
-}
-
-- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
-{
-    UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
-    return attributes;
+    
+    return answer;
+    
 }
 
 @end
