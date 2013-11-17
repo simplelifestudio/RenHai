26f4f9235fbd9909f4b1a62f29dbf6f86cb397ea
Patrick Deng <simplelife.patrick@gmail.com>
2013-11-17 10:29:06 +0800

    新增AutoScrollLabel库

--- Codes/iOS/RenHai/Pods/Pods-environment.h
+++ Codes/iOS/RenHai/Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 3
 #define COCOAPODS_VERSION_PATCH_AFNetworking 3
 
+// AutoScrollLabel
+#define COCOAPODS_POD_AVAILABLE_AutoScrollLabel
+#define COCOAPODS_VERSION_MAJOR_AutoScrollLabel 0
+#define COCOAPODS_VERSION_MINOR_AutoScrollLabel 2
+#define COCOAPODS_VERSION_PATCH_AutoScrollLabel 1
+
 // CWPopup
 #define COCOAPODS_POD_AVAILABLE_CWPopup
 #define COCOAPODS_VERSION_MAJOR_CWPopup 1
@@ -22,7 +28,19 @@
 #define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
 #define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
 #define COCOAPODS_VERSION_MINOR_CocoaLumberjack 6
-#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 2
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 3
+
+// CocoaLumberjack/Core
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Core
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Core 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Core 6
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Core 3
+
+// CocoaLumberjack/Extensions
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Extensions
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Extensions 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 6
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Extensions 3
 
 // DraggableCollectionView
 #define COCOAPODS_POD_AVAILABLE_DraggableCollectionView
@@ -58,7 +76,7 @@
 #define COCOAPODS_POD_AVAILABLE_PKRevealController
 #define COCOAPODS_VERSION_MAJOR_PKRevealController 2
 #define COCOAPODS_VERSION_MINOR_PKRevealController 0
-#define COCOAPODS_VERSION_PATCH_PKRevealController 5
+#define COCOAPODS_VERSION_PATCH_PKRevealController 6
 
 // Reachability
 #define COCOAPODS_POD_AVAILABLE_Reachability
