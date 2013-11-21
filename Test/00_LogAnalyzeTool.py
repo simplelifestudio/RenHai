5c71780fa453eaf5c7f4d8155b8eb2b5a9154aca
Tomas <simplelife.tomas@gmail.com>
2013-11-21 20:09:13 +0800

    新增：测试脚本

---
+++ Test/00_LogAnalyzeTool.py
@@ -0,0 +1,26 @@
+import sys
+
+def containsAny(keys, line):
+    """ Check whether sequence seq contains ANY of the items in aset. """
+    for c in keys:
+        if c in line:
+            return True
+    return False
+
+def main(argv):
+    inputfile = argv[1]
+    output = argv[2]
+    keys = argv[3].split(',')
+
+    outputfile = open(output, 'w')
+    with open(inputfile) as file:
+        for line in file:
+            if (containsAny(keys, line)):
+                outputfile.write(line)
+    outputfile.close()
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1):
+        print("Parameters missed, usage of this tool:\n  LogAnalyzeTool.py inputfile outputfile keys")
+        sys.exit(1)
+    main(sys.argv)
