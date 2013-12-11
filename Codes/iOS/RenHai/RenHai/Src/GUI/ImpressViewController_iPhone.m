1394e52ac54705a352bffc926e404ae52033b6d2
Patrick Deng <simplelife.patrick@gmail.com>
2013-12-11 13:57:34 +0800

    改进ImpressCard界面布局

--- Codes/iOS/RenHai/RenHai/Src/GUI/ImpressViewController_iPhone.m
+++ Codes/iOS/RenHai/RenHai/Src/GUI/ImpressViewController_iPhone.m
@@ -318,9 +318,15 @@
         reusableView = headerView;        
     }
     
-    
+    reusableView.backgroundColor = FLATUI_COLOR_MAJOR_F;
+    DDLogVerbose(@"#####resuableView height: %f", reusableView.frame.size.height);
     return reusableView;
 }
+
+//- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section
+//{
+//
+//}
 
 #pragma mark - UIScrollViewDelegate
 
