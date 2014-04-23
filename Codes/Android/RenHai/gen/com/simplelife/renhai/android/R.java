fc99e7088af54601265e563efdd112e24667a96b
Chris Li <simplelife.chris@gmail.com>
2014-04-23 23:13:41 +0800

    开始聊天界面框架填充

--- Codes/Android/RenHai/gen/com/simplelife/renhai/android/R.java
+++ Codes/Android/RenHai/gen/com/simplelife/renhai/android/R.java
@@ -499,26 +499,29 @@ public final class R {
         public static final int spinner_ab_focused_actionbarwithtabstheme=0x7f02001a;
         public static final int spinner_ab_pressed_actionbarwithtabstheme=0x7f02001b;
         public static final int spinner_background_ab_actionbarwithtabstheme=0x7f02001c;
-        public static final int tab_indicator_ab_actionbarwithtabstheme=0x7f02001d;
-        public static final int tab_selected_actionbarwithtabstheme=0x7f02001e;
-        public static final int tab_selected_focused_actionbarwithtabstheme=0x7f02001f;
-        public static final int tab_selected_pressed_actionbarwithtabstheme=0x7f020020;
-        public static final int tab_unselected_actionbarwithtabstheme=0x7f020021;
-        public static final int tab_unselected_focused_actionbarwithtabstheme=0x7f020022;
-        public static final int tab_unselected_pressed_actionbarwithtabstheme=0x7f020023;
+        public static final int startvedio_rectangle=0x7f02001d;
+        public static final int tab_indicator_ab_actionbarwithtabstheme=0x7f02001e;
+        public static final int tab_selected_actionbarwithtabstheme=0x7f02001f;
+        public static final int tab_selected_focused_actionbarwithtabstheme=0x7f020020;
+        public static final int tab_selected_pressed_actionbarwithtabstheme=0x7f020021;
+        public static final int tab_unselected_actionbarwithtabstheme=0x7f020022;
+        public static final int tab_unselected_focused_actionbarwithtabstheme=0x7f020023;
+        public static final int tab_unselected_pressed_actionbarwithtabstheme=0x7f020024;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int action_settings=0x7f090006;
         public static final int mainpage_viewpager=0x7f090000;
         public static final int protocal_btnno=0x7f090003;
         public static final int protocal_btnyes=0x7f090002;
         public static final int protocal_confirm=0x7f090001;
         public static final int splashteamlogo=0x7f090004;
+        public static final int startvedio_onlinecount=0x7f090005;
     }
     public static final class layout {
         public static final int activity_mainpage_viewpager=0x7f030000;
         public static final int activity_protocal=0x7f030001;
         public static final int activity_splash=0x7f030002;
+        public static final int fragment_startvedio=0x7f030003;
     }
     public static final class menu {
         public static final int splash=0x7f080000;
--- Codes/Android/RenHai/src/com/simplelife/renhai/android/RenHaiStartVedioFragment.java
+++ Codes/Android/RenHai/src/com/simplelife/renhai/android/RenHaiStartVedioFragment.java
@@ -8,10 +8,27 @@
  */
 package com.simplelife.renhai.android;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class RenHaiStartVedioFragment extends Fragment {
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);	
+	}
 	
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+    	
+    	View rootView = inflater.inflate(R.layout.fragment_startvedio, container, false);
+    	
+    	return rootView;
+    
+    }
 
 }
