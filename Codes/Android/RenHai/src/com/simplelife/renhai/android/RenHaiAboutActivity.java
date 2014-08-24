86f2607c8d0622e4228644baeda480213cc6238e
Chris Li <simplelife.chris@gmail.com>
2014-08-24 22:57:23 +0800

    定制设置页面的检查更新选项

--- Codes/Android/RenHai/src/com/simplelife/renhai/android/RenHaiAboutActivity.java
+++ Codes/Android/RenHai/src/com/simplelife/renhai/android/RenHaiAboutActivity.java
@@ -2,15 +2,24 @@ package com.simplelife.renhai.android;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class RenHaiAboutActivity extends Activity {
+	
+	private TextView mVersion;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
    	    super.onCreate(savedInstanceState);
    	    setContentView(R.layout.activity_about);
+   	    mVersion = (TextView) findViewById(R.id.about_version);
+   	    
+   	    mVersion.setText(getVersion());
+   	    
    	    // Set a title for this page
 		ActionBar tActionBar = getActionBar();
 		tActionBar.setTitle(R.string.config_aboutpage_title);
@@ -28,5 +37,17 @@ public class RenHaiAboutActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+	
+	public String getVersion() {
+	     try {
+	         PackageManager manager = this.getPackageManager();
+	         PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+	         String version = info.versionName;
+	         return this.getString(R.string.config_aboutpage_version) + version;
+	     } catch (Exception e) {
+	         e.printStackTrace();
+	         return this.getString(R.string.config_aboutpage_versionerror);
+	     }
+	 }
 
 }
--- Codes/Android/RenHai/src/com/simplelife/renhai/android/RenHaiConfigActivity.java
+++ Codes/Android/RenHai/src/com/simplelife/renhai/android/RenHaiConfigActivity.java
@@ -1,9 +1,13 @@
 package com.simplelife.renhai.android;
 
+import com.simplelife.renhai.android.ui.RenHaiBadgeView;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -11,9 +15,14 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 
 public class RenHaiConfigActivity extends Activity {
+	
+	private static RenHaiBadgeView badge1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +56,9 @@ public class RenHaiConfigActivity extends Activity {
 		
 		private SharedPreferences mSharedPrefs;
 		private Preference mPrefAbout;
+		private Preference mPrefUpdate;
+		
+		private TextView mVersion;
 
 		@Override  
         public void onCreate(Bundle savedInstanceState) {    
@@ -56,8 +68,14 @@ public class RenHaiConfigActivity extends Activity {
             mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
             
             mPrefAbout = (Preference)findPreference("config_about");
+            mPrefUpdate = (Preference)findPreference("config_update");           
             
             mPrefAbout.setOnPreferenceClickListener(this);
+            mPrefUpdate.setOnPreferenceClickListener(this);
+            
+            mVersion = (TextView) getActivity().findViewById(R.id.config_updateversion);
+            //int tLayout = mPrefUpdate.getLayoutResource();
+            //mVersion.setText(getVersion());
 		}
 		
 		@Override
@@ -66,6 +84,9 @@ public class RenHaiConfigActivity extends Activity {
 			if(mPrefAbout == preference)
         	{
 				onShowAboutPage();
+        	}else if(mPrefUpdate == preference)
+        	{
+        		
         	}
 			return false;
 		}
@@ -80,6 +101,20 @@ public class RenHaiConfigActivity extends Activity {
 			Intent tIntent = new Intent(getActivity(), RenHaiAboutActivity.class);
     		startActivity(tIntent);  
 		}
+		
+		public String getVersion() {
+		     try {
+		         PackageManager manager = getActivity().getPackageManager();
+		         PackageInfo info = manager.getPackageInfo(getActivity().getPackageName(), 0);
+		         String version = info.versionName;
+		         return this.getString(R.string.config_aboutpage_version) + version;
+		     } catch (Exception e) {
+		         e.printStackTrace();
+		         return this.getString(R.string.config_aboutpage_versionerror);
+		     }
+		 }
 	}
+	
+	
 
 }
